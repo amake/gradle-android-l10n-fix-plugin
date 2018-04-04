9689cd574a4992f5774e91325cd2d0f00cd9e82c
Aaron Madlon-Kay <aaron@madlon-kay.com>
2018-04-05 00:45:01 +0900

    Plugin skeleton

---
+++ src/main/java/com/madlonkay/android/L10nFixPlugin.java
@@ -0,0 +1,11 @@
+package com.madlonkay.android;
+
+import com.android.build.gradle.internal.tasks.AndroidBuilderTask;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class L10nFixPlugin implements Plugin<Project> {
+    public void apply(Project project) {
+    }
+}
