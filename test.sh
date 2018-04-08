1d150b379f9b5dcadbfe07ff2460d37728d819c5
Aaron Madlon-Kay <aaron@madlon-kay.com>
2018-04-08 21:02:38 +0900

    Add test for generated string array

--- test.sh
+++ test.sh
@@ -15,4 +15,8 @@ function die() {
 $aapt d --values resources app/build/outputs/apk/debug/app-debug.apk resources.arsc |
     grep -F "type 10 configCount=5" || die "APK had wrong number of language resources"
 
+grep -F 'SUPPORTED_LOCALES = { "es-MX", "fr", "ja", "sr-Latn" };' \
+    app/build/generated/source/buildConfig/debug/org/madlonkay/testapp/BuildConfig.java ||
+    die "BuildConfig.SUPPORTED_LOCALES is missing or incorrect"
+
 echo "Tests complete"
