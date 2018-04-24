0330d6531bbd0feb821d475973c261f2a1a2d726
Aaron Madlon-Kay <aaron@madlon-kay.com>
2018-04-25 01:46:56 +0900

    Use generated activity in integration app

--- integration/app/src/main/java/org/madlonkay/testapp/MainActivity.java
+++ integration/app/src/main/java/org/madlonkay/testapp/MainActivity.java
@@ -1,9 +1,9 @@
 package org.madlonkay.testapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends L10nActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
