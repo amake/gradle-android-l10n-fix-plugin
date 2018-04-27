142f181ab7a970aea64a4b3ff4129e65dd73c9ba
Aaron Madlon-Kay <aaron@madlon-kay.com>
2018-04-27 13:43:12 +0900

    Include stack trace in test failures

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 cd integration
-../gradlew -Pl10nFixVerbosity=1 clean assembleDebug test
+../gradlew -Pl10nFixVerbosity=1 clean assembleDebug test --stacktrace
 
 aapt=$ANDROID_HOME/build-tools/$(ls $ANDROID_HOME/build-tools | tail -n 1)/aapt
 
