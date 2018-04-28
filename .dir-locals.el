78316e5ac4b7637efa5d38cb58b7ce4c710513fb
Aaron Madlon-Kay <aaron@madlon-kay.com>
2018-04-28 22:23:48 +0900

    Set emacs flycheck-gradle-java-compile-function

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((java-mode
+  (flycheck-gradle-java-compile-function . flycheck-gradle-java-compile->android)))
